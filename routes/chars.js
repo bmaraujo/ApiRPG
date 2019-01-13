b2c644f6088dd176a102658b8ff08a905c3fe565
Bruno Araujo <bruno.mourao.araujo@gmail.com>
2019-01-13 09:54:14 -0200

    Added treatment when a char or ability are not found.

--- routes/chars.js
+++ routes/chars.js
@@ -25,26 +25,44 @@ router.get('/', function(req, res, next) {
 /* GET Char*/
 router.get('/:charName', function(req, res, next) {
 	let char = getChar(req.params.charName);
+
 	return res.status(200).json({success:true, data: char});
 });
 
 /* GET Char HP */
 router.get('/:charName/HP', function(req, res, next) {
 	let char = getChar(req.params.charName);
-	return res.status(200).json({success:true, data: char.HP});
+	if(char){
+		return res.status(200).json({success:true, data: char.HP});
+	}
+	else{
+		charNotFound(res,req.params.charName);
+	}
 });
 
 /* GET Char Ability (STR,DEX,CON,INT,WIS,CHA) */
 router.get('/:charName/Ability/:ability', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	let ability = getAbility(char, req.params.ability);
+	if(!ability){
+		abilityNotFound(res,req.params.ability);
+	}
 	return res.status(200).json({success:true, data: ability.value});
 });
 
 /* GET Char Ability (STR,DEX,CON,INT,WIS,CHA) modifier */
 router.get('/:charName/Ability/:ability/mod', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	let ability = getAbility(char, req.params.ability);
+	if(!ability){
+		abilityNotFound(res,req.params.ability);
+	}
 	let mod = Math.floor((parseInt(ability.value) - 10)/2);
 	if(mod > 0){
 		mod = "+" + mod;
@@ -55,24 +73,36 @@ router.get('/:charName/Ability/:ability/mod', function(req, res, next) {
 /* GET Char AC */
 router.get('/:charName/AC', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	return res.status(200).json({success:true, data: char.AC});
 });
 
 /* GET Char Initiative */
 router.get('/:charName/Initiative', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	return res.status(200).json({success:true, data: char.Initiative});
 });
 
 /* GET Char Proeficiency Bonus */
 router.get('/:charName/Proeficiency', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	return res.status(200).json({success:true, data: char.Proeficiency});
 });
 
 /* GET Char Hit Die */
 router.get('/:charName/HitDie', function(req, res, next) {
 	let char = getChar(req.params.charName);
+	if(!char){
+		charNotFound(res,req.params.charName);
+	}
 	return res.status(200).json({success:true, data: char.HitDie});
 });
 
@@ -81,7 +111,6 @@ module.exports = router;
 function getAllChars(){
 	console.log("opening chars.json");
 	return JSON.parse(fs.readFileSync('./chars/chars.json'));
-	console.log(JSON.stringify(chars));
 }
 
 function getChar(name){
@@ -113,4 +142,12 @@ function getAbility(char,ability){
 	}
 	console.log(`Found: ${slctdAbility}`);
 	return slctdAbility;
+}
+
+function charNotFound(res,charName){
+	return res.status(404).json({success:false,data:`Não foi possível encontrar o personagem ${charName}. Tá certo o nome?`});
+}
+
+function abilityNotFound(res,ability){
+	return res.status(404).json({success:false,data:`Não foi possível encontrar a habilidade ${ability}. Os valores possíveis são: STR, DEX, CON, INT, WIS e CHA.`});
 }
