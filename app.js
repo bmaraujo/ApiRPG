66ad79097827f475718c1c83523145978c21afdf
Bruno Mourão de Araújo <brunom@take.net>
2019-01-24 14:39:30 -0200

    Added roll feature

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var chars = require('./routes/chars');
+var roll = require('./routes/roll');
 
 var app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/v1/chars', chars);
+app.use('/v1/roll', roll);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/roll.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var router = express.Router();
+
+
+router.get('/', function(req, res, next){
+    
+	let rpgDice = req.query.dice;
+
+	let posD = rpgDice.indexOf('d');
+    let qtdDados = rpgDice.substring(0,posD);
+    let posMod = rpgDice.indexOf("+");
+    if(posMod <0){
+        posMod = rpgDice.indexOf("-");
+    }
+    let qtdLados = 0;
+    let modificador = 0;
+    if(posMod >=0){
+    	qtdLados = rpgDice.substring(posD+1,posMod);
+        modificador = rpgDice.substring(posMod);
+    }
+    else{
+    	qtdLados = rpgDice.substring(posD+1);
+    }
+
+    let resposta = {total:"0",dados:[]};
+    let total =0;
+    for(let i=0;i<qtdDados; i++){
+        let roll = Math.ceil(Math.random() * qtdLados);
+        total += roll;
+        resposta.dados.push(roll);
+    }
+    
+    if(modificador){
+    	total +=  parseInt(modificador);
+    }
+    
+    resposta.total = total;
+
+    return res.status(200).json({success:true, data: resposta});
+});
+
+module.exports = router;
